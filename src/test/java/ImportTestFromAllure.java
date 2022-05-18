54fd74a1ca40e90c2233d2cb2cf5c55d2f3e2e2b
dmitriy.tokarev <dmitriy.tokarev@quadcode.com>
2022-05-18 22:22:22 +0300

    commit #1

--- src/test/java/ImportTestFromAllure.java
+++ src/test/java/ImportTestFromAllure.java
@@ -1,9 +1,42 @@
-import io.qameta.allure.Description;
-import io.qameta.allure.Feature;
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 
 public class ImportTestFromAllure {
     @Test
+    @AllureId("9996")
+    @DisplayName("Login Test")
+    @Tags({@Tag("average"), @Tag("integration"), @Tag("asap"), @Tag("login")})
+    @Owner("allure8")
     void loginTestImportedFromAllureTestOps() {
+        step("Open login page", () -> {
+            open("https://url.url");
+        });
+        step("Enter user_name", () -> {
+            $("#name").setValue("admin");
+        });
+        step("Enter user_password", () -> {
+            $("#password").setValue("123");
+        });
+        step("Click button \"Submit\"", () -> {
+            $("#submit").click();
+        });
+
+        step("Check ER:", () -> {
+            step("User has logged", () ->
+                    $("#header").shouldHave(Condition.exactText("User has logged"));
+        });
+        step("In main menu-bar user_name = Admin", () -> {
+            $("#user-bar").shouldHave(Condition.exactText("Admin"));
+        });
+    });
+}
 }
