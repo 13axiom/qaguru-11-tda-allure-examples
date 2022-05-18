066b5c0816c578c4063a466b7e6c0dee574b16cf
dmitriy.tokarev <dmitriy.tokarev@quadcode.com>
2022-05-18 22:03:18 +0300

    init commit

---
+++ src/test/java/ImportTestFromAllure.java
@@ -0,0 +1,9 @@
+import io.qameta.allure.Description;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ImportTestFromAllure {
+    @Test
+    void loginTestImportedFromAllureTestOps() {
+}
